ed81dc03a2b9f40e82ed25144a03ac608d7921ab
Nico-toulouse <nicoramon@hotmail.com>
2020-03-26 16:44:28 +0100

    map 1 solved

--- map/ex1.js
+++ map/ex1.js
@@ -26,8 +26,11 @@ Expected OUTPUT for this sample array:
 
 */
 
+
 function getStringsLength(strings) {
+  return strings.map(str => str = `${str} contains ${str.length} characters`)
 }
+
 
 // If you need, you can always visualize the result with a console.log.
 // In that case, uncomment the line below (you can also add your own
